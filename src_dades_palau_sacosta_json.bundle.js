1f11a6c1f17574a109fa818466bef7e4c5a0016f
CLlauger <50671982+CLlauger@users.noreply.github.com>
2025-08-30 21:14:30 +0200

    Updates

---
+++ src_dades_palau_sacosta_json.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkpadrons_catalunya"] = self["webpackChunkpadrons_catalunya"] || []).push([["src_dades_palau_sacosta_json"],{
+
+/***/ "./src/dades/palau_sacosta.json":
+/*!**************************************!*\
+  !*** ./src/dades/palau_sacosta.json ***!
+  \**************************************/
+/***/ ((module) => {
+
+eval("module.exports = /*#__PURE__*/JSON.parse('[{\"any\":1940,\"categoria\":\"padró\",\"id\":\"0003548272\",\"carrers\":[{\"nom\":\"Portada\",\"via\":\"\",\"nom_es\":\"\",\"pagines\":[3,62]},{\"nom\":\"Barcelona\",\"via\":\"Carretera de \",\"nom_es\":\"Barcelona\",\"pagines\":[4,5,6,7,8,9,10,11,12,13,14,15]},{\"nom\":\"Bòbila d\\'en Ginesta\",\"via\":\"Carrer de la \",\"nom_es\":\"Bóvila Ginesta\",\"pagines\":[24,25,26,27]},{\"nom\":\"Despoblat\",\"via\":\"\",\"nom_es\":\"Despoblado\",\"pagines\":[42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61]},{\"nom\":\"Generalíssim\",\"via\":\"Avinguda del \",\"nom_es\":\"Generalisimo\",\"pagines\":[14,15,16,17,18,19,20,21]},{\"nom\":\"Marquès de Caldes de Montbui\",\"via\":\"Carrer del \",\"nom_es\":\"Marqués Caldas Montbuy\",\"pagines\":[26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43]},{\"nom\":\"Sant Isidre\",\"via\":\"Carrer de \",\"nom_es\":\"San Isidro\",\"pagines\":[20,21,22,23]},{\"nom\":\"Santa Coloma\",\"via\":\"Carretera de \",\"nom_es\":\"Santa Coloma\",\"pagines\":[22,23,24,25]},{\"nom\":\"Miquel Vich\",\"via\":\"Caseriu de \",\"nom_es\":\"Miguel Vich\",\"pagines\":[24,25]}]}]');\n\n//# sourceURL=webpack://padrons-catalunya/./src/dades/palau_sacosta.json?");
+
+/***/ })
+
+}]);
